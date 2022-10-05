96562da6e9b727660d31f87399c140277cd12233
koto <2650006147@qq.com>
2022-10-06 02:58:34 +0800

    asd

--- app/router.js
+++ app/router.js
@@ -6,5 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/detail',controller.home.detail)
   router.post('/api/user/register',controller.user.register);
 };
